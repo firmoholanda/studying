84f6bbe7f9161e2379013dc4b535a66bcb826cc1
DiegoServinHdn <54914983+DiegoServinHdn@users.noreply.github.com>
2019-09-04 16:34:31 -0500

    i create utopian tree

---
+++ warmup/Utopian_Tree.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
