d087570b425692a9e4dfd4ff801f2c0340938d45
firmo holanda <firmo@viize.com>
2019-09-26 09:36:06 -0300

    removeFromArray done

--- theodinproject/web development 101/assignments/javascript-exercises-master/removeFromArray/removeFromArray.js
+++ theodinproject/web development 101/assignments/javascript-exercises-master/removeFromArray/removeFromArray.js
@@ -1,11 +1,4 @@
 const removeFromArray = function(arr, item1, item2, item3, item4) {
-    
-    //console.log(item1);
-    //console.log(item2);
-    //console.log(item3);
-    //console.log(item4);
-    //console.log(typeof item4 == "undefined");
-
     let items = [];
     
     if (typeof item1 != "undefined") {items.push(item1);}
@@ -14,8 +7,6 @@ const removeFromArray = function(arr, item1, item2, item3, item4) {
     if (typeof item4 != "undefined") {items.push(item4);}
 
     for (i = 0; i < items.length; i++) {
-
-        console.log(arr.indexOf(items[i]));
         var index = arr.indexOf(items[i]);
         if (index > -1) {
             arr.splice(index, 1);
@@ -28,4 +19,4 @@ const removeFromArray = function(arr, item1, item2, item3, item4) {
 //---------------------------------------------------------------------------------------
 module.exports = removeFromArray
 
-console.log( removeFromArray([1, 2, 3, 4], 3, 4) );
+console.log( removeFromArray([1, 2, 3, 4], 3) );
