b066fb1cc0dc859f210c31e4dfa8d1a79ede3029
firmo holanda <firmo@viize.com>
2019-09-26 10:13:19 -0300

    sumAll complete

--- theodinproject/web development 101/assignments/javascript-exercises-master/sumAll/sumAll.js
+++ theodinproject/web development 101/assignments/javascript-exercises-master/sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+
+    //if any number is not a number
+    if ( (typeof n1 != "number") || (typeof n2 != "number") ) {
+        return "ERROR";
+    }
+    //if any number is negative
+    if ( (n1 * n2) < 0 ) {
+        return "ERROR";
+    }
+    //if any number are equal
+    else if (n1 == n2) {                    
+        return n1;
+    }
+    //left < right, recursion up
+    else if (n1 < n2) {
+        return n1 + sumAll(n1+1, n2);
+    }
+    //left > right, recursion down
+    else {
+        return n1 + sumAll(n1-1, n2);
+    } 
 
 }
 
 module.exports = sumAll
+
+
+console.log( sumAll(10, "10") );
