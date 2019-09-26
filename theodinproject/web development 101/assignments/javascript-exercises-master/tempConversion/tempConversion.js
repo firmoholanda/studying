0013c7c8f57932ebe135a567c4043262eb2a311e
firmo holanda <firmo@viize.com>
2019-09-26 10:54:19 -0300

    tempConversion done

--- theodinproject/web development 101/assignments/javascript-exercises-master/tempConversion/tempConversion.js
+++ theodinproject/web development 101/assignments/javascript-exercises-master/tempConversion/tempConversion.js
@@ -1,12 +1,24 @@
-const ftoc = function() {
 
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
 }
 
-const ctof = function() {
+const ftoc = function(temp) {
+    const result = (temp - 32) * (5/9);
+    return  round(result, 1);
+}
 
+const ctof = function(temp) {
+    const result = (temp * 9/5) + 32 ;
+    return round(result, 1);
 }
 
 module.exports = {
   ftoc,
   ctof
 }
+//---------------------------------------------------------------------------------------
+
+//console.log(ftoc(32) );
+console.log( ctof(73.2) );
