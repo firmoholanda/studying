5036f266bb894603fadf86091dcf7a640d44df37
firmo holanda <firmo@viize.com>
2019-10-16 14:14:21 -0300

    up

--- theodinproject/odin_on_rails/my_first_rails_app/config/routes.rb
+++ theodinproject/odin_on_rails/my_first_rails_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-    root 'cars#index'
-    resources :cars
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cars#index'
+  resources :cars
+end
