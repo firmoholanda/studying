e696b2af8ab48a2eac94ef0c4046131cd8875aa9
firmo holanda <firmo@viize.com>
2019-12-06 20:02:37 -0300

    Update electronicsShop.rb

--- hackerrank/electronicsShop.rb
+++ hackerrank/electronicsShop.rb
@@ -2,27 +2,29 @@
 #!/bin/ruby
 
 def getMoneySpent(keyboards, drives, b)
-  herbudget = b
-  if (keyboards.min + drives.min) > herbudget
+  herBudget = b
+  
+  if (keyboards.min + drives.min) > herBudget
     return -1
   else
     if keyboards.max > drives.max
       drives.each do |dItem|
-        if (dItem + keyboards.max) < herbudget
+        if (dItem + keyboards.max) < herBudget
           return dItem + keyboards.max
         end
       end
     else
-      keyboards.each do |bkItem|
-        if (bkItem + drives.max) < herbudget
-          return bkItem + drives.max
+      keyboards.each do |kbItem|
+        if (kbItem + drives.max) < herBudget
+          return kbItem + drives.max
         end
       end
     end
   end
+
 end
 
-b = [10, 2, 3]
+b = 10
 keyboards = [3, 1] 
 drives = [5, 2, 8]
 
