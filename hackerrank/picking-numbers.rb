cc0749727560102b960141f2924f6812cbc1d533
firmo holanda <firmo@viize.com>
2019-12-11 20:21:26 -0300

    up

---
+++ hackerrank/picking-numbers.rb
@@ -0,0 +1,17 @@
+#https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/picking-numbers
+
+def pickingNumbers(a)
+  
+  my_a = []
+
+  #a.each do |i|
+  #  my_sub_a = a.delete_at(i)
+  #  puts a
+    
+  end
+
+  #puts my_a
+
+end
+
+pickingNumbers([4, 6, 5, 3, 3, 1])
