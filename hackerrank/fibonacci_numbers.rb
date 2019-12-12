5f35961cec7feca105156c1fe4ff8e6cf42e1352
firmo holanda <firmo@viize.com>
2019-12-12 14:12:08 -0300

    Update fibonacci_numbers.rb

--- hackerrank/fibonacci_numbers.rb
+++ hackerrank/fibonacci_numbers.rb
@@ -7,22 +7,18 @@ def fib(n)
 end
 
 def fibEven(n)
-
   i = 0
   while fib(i+1) <= n do
-    my_arr = (2..i+1).map { |x| fib(x) }
-    my_arr_even = []
-    my_arr.map { |i| my_arr_even.push(i) if i.even? }
     i += 1
   end
+  my_arr = (2..i+1).map { |x| fib(x) }
+  my_arr_even = []
+  my_arr.map { |i| my_arr_even.push(i) if i.even? }
 
-  #puts i
-  #puts fib(i)
-  #puts my_arr
-  puts my_arr_even
-  puts "result: " + my_arr_even.sum.to_s
+  return my_arr_even.sum
 end
 
-fibEven(100)
-
-#puts fib(100)
+puts fibEven(100)
+#puts i
+  #puts fib(i)
+  #puts my_arr
