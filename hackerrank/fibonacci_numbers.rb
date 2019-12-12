4d99dbc5f25441760241112247559fe710bf86b7
firmo holanda <firmo@viize.com>
2019-12-12 17:20:22 -0300

    finished my fib

--- hackerrank/fibonacci_numbers.rb
+++ hackerrank/fibonacci_numbers.rb
@@ -7,24 +7,30 @@
 #  @cache[n] = fib(n-1) + fib(n-2)
 #end
 
-def fib(n)
-  phi = (Math.sqrt(5) + 1) / 2
-  return ((phi**n) / Math.sqrt(5)).round
-end
+#def fib(n)
+#  phi = (Math.sqrt(5) + 1) / 2
+#  return ((phi**n) / Math.sqrt(5)).round
+#end
 
 def fibEven(n)
-  i = 0
-  while fib(i) <= n do
-    i += 1
+
+  # creates fibonacci array
+  my_fib = [0, 1]
+  (2..n).each do |i|
+    (my_fib[-1] > n) ? break : my_fib.push(my_fib[i-1] + my_fib[i-2])    
   end
-  my_arr = (1..i-1).map { |x| fib(x) }
-  my_arr_even = []
-  my_arr.map { |i| my_arr_even.push(i) if i.even? }
 
-  #puts my_arr
-  #puts my_arr_even
+  # remove item if greater than n (need work)
+  my_fib.each do |i|
+    my_fib.pop if (i > n)
+  end
 
-  return my_arr_even.sum
+  # select even numbers
+  my_fib_even = []
+  my_fib.map { |i| my_fib_even.push(i) if i.even? }
+
+  return my_fib_even.sum
+
 end
 
 puts fibEven(100)
