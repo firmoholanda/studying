91f3bfb474c8eee7ba7123f7e68656bf61edf2c5
firmo holanda <firmo@viize.com>
2019-12-16 08:29:56 -0300

    up

---
+++ microverse/course/projects/02 ruby/ruby-enumerable-methods/readme.md
@@ -0,0 +1,61 @@
+# 📃 ruby-enumerable-methods
+
+this project is part of a series of projects to be completed by students of [Microverse](https://www.microverse.org/ 'The Global School for Remote Software Developers!').
+
+on this project we are going to rebuild some ruby built-in enumerable methods by hand.
+
+
+
+## 🔗 link to assignment
+
+the assignment can be found in the Odin Project's HTML and CSS course that can be found [here](https://www.theodinproject.com/courses/ruby-programming/lessons/advanced-building-blocks).
+
+
+
+## 📡 technologies used
+
+- ruby
+- github
+
+basic ruby and github was used on this project.
+
+
+
+## 🚀 the project
+
+this project can be found in https://github.com/firmoholanda/ruby-enumerable-methods
+
+### ✨ [code picture]
+
+<img alt="page animation" src="https://github.com/firmoholanda/ruby-enumerable-methods/blob/development/img/ruby-enumerable-methods.png"/>
+
+
+
+## 🤝 contributing
+
+contributions, issues and feature requests are welcome!<br/>Feel free to check [issues page](https://github.com/firmoholanda/ruby-enumerable-methods/development/issues).
+
+1. fork it (https://github.com/firmoholanda/ruby-enumerable-methods/development/fork)
+2. create your working branch (git checkout -b [choose-a-name])
+3. commit your changes (git commit -am 'what this commit will fix/add/improve')
+4. push to the branch (git push origin [chosen-name])
+5. create a new pull request
+
+
+
+## 🤖 contributor
+
+firmo holanda - [GitHub](https://github.com/firmoholanda)
+
+
+
+## 🙋‍♂ show your support
+
+give a ⭐️ if you like this project!
+
+
+
+## 📝 license
+
+this project is [MIT](https://github.com/firmoholanda/newsweek.com-clone/development/blob/development/license.txt) licensed.
+ 
---
+++ microverse/course/projects/02 ruby/tic-tac-toe/bin/main.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+
+require '../lib/game_logic.rb'
+
+# UserInterface --------------------------------------------------------------------------- #
+class UserInterface
+  attr_reader :user_interface
+
+  def initialize
+    @user_interface = user_interface
+  end
+
+  def instructions
+    puts "game instructions: "
+    puts "use 1-9 keys to make your turn.\n\n"
+    puts "1|2|3"
+    puts "4|5|6"
+    puts "7|8|9"
+  end
+
+  def get_names(players)
+    names = []
+    players.each_with_index do |player, i|
+      puts "\n\nplease enter the name of player #{i+1}:"
+      name = gets.chomp.downcase
+      while name == ""
+        puts "invalid input. please enter a valid name:"
+        name = gets.chomp.downcase
+      end
+      names << name
+    end
+    names
+  end
+
+  def display(board)
+    puts board[0..2].join("|")
+    puts board[3..5].join("|")
+    puts board[6..8].join("|")
+  end
+
+  def assign_piece(players)
+    puts "\n\n#{players[0].name} has been randomly assigned to #{players[0].team}."
+    puts "therefore, #{players[1].name} has been assigned to #{players[1].team}."
+  end
+
+  def first_turn(player)
+    puts "\nThe computer has randomly decided that #{player.name} will go first.\n\n"
+  end
+
+  def get_turn(player, board)
+    puts "\n#{player.name}, please take your turn:"
+    turn = gets.chomp
+    until input_valid?(turn) && board.move_valid?(turn.to_i)
+      puts "invalid input. Please use keys 1-9 to choose an empty position.\n\n"
+      turn = gets.chomp
+    end
+    turn.to_i
+  end
+
+  def input_valid?(turn)
+    ("1".."9").to_a.include?(turn)
+  end
+
+  def declare_win(player, board)
+    puts "\ngame over!"
+    board.win?(player.team) ? (puts "\ncongratulations, #{player.name}!") : (puts "\nit's a tie!")
+  end
+
+  #def play_new_game?
+  #  until
+  #    puts "\ndo you want to play a new game? (y or n)"
+  #    new_game = gets.chomp
+  #  end
+  #end
+
+end
+
+# start the game ---------------------------------------------------------------------- #
+
+#game = Game.new
+#game.play
+
+
+user_interface = UserInterface.new
+
+user_interface.instructions
