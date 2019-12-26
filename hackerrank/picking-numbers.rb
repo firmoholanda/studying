0360737229a15347620bfbcdfa8d6a93db00aa85
firmo holanda <firmo@viize.com>
2019-12-26 19:14:41 -0300

    update electronics_shop

--- hackerrank/picking-numbers.rb
+++ hackerrank/picking-numbers.rb
@@ -2,16 +2,24 @@
 
 def pickingNumbers(a)
   
-  my_a = []
+  my_arr = [[]]
+  uniq_arr = a.uniq
 
-  #a.each do |i|
-  #  my_sub_a = a.delete_at(i)
-  #  puts a
-    
+  
+  
+  #a.product(uniq_arr).map { |x, y| my_arr.push([x, y]) if (x-y).abs < 1}
+  #p uniq_arr.product(a)
+
+  uniq_arr.length.times do |i|
+    a.length.times do |j|
+      if  (uniq_arr[i] - a[j]).abs <= 1 
+        my_arr[i][j].push("k")
+      end
+    end
   end
 
-  #puts my_a
+  #p my_arr
 
 end
 
-pickingNumbers([4, 6, 5, 3, 3, 1])
+pickingNumbers([1, 1, 2, 2, 4, 4, 5, 5])
