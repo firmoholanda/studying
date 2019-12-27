9cafe8d8e2606c87f252b5d0152dc71776d59068
firmo holanda <firmo@viize.com>
2019-12-27 18:22:54 -0300

    Update picking-numbers.rb

--- hackerrank/picking-numbers.rb
+++ hackerrank/picking-numbers.rb
@@ -20,35 +20,17 @@ def pickingNumbers(a)
     if (v.max - v.min) <= 1 then return_arr.push(v.length) end
   end
 
-  
-  my_hash.each do |key, value|
-    puts key.to_s + ' : ' + value.sort.to_s
-  end
-  p return_arr
+  p a
+  #my_hash.each do |key, value|
+  #  puts key.to_s + ' : ' + value.sort.to_s
+  #end
+  #p return_arr
   #return return_arr.max
 
 end
 # ------------------------------------------------------------------------------------- #
 
-#pickingNumbers([1, 2, 2, 3, 1, 2])
-pickingNumbers([84, 43, 11, 41, 2, 99, 31, 32, 56, 53, 42, 14, 98, 27, 64, 57, 16, 33, 48, 21, 46, 61, 87, 90, 28, 12, 62, 49, 29, 77, 82, 70, 80, 89, 95, 52, 13, 19, 9, 79, 35, 67, 51, 39, 7, 1, 66, 8, 17, 85, 71, 97, 34, 73, 75, 6, 91, 40, 96, 65, 37, 74, 20, 68, 23, 47, 76, 55, 24, 3, 30, 22, 55, 5, 69, 86, 54, 50, 10, 59, 15, 4, 36, 38, 83, 60, 72, 63, 78, 58, 88, 93, 45, 18, 94, 44, 92, 81, 25, 26])
+pickingNumbers([1, 2, 2, 3, 1, 2])
+
+#pickingNumbers([84, 43, 11, 41, 2, 99, 31, 32, 56, 53, 42, 14, 98, 27, 64, 57, 16, 33, 48, 21, 46, 61, 87, 90, 28, 12, 62, 49, 29, 77, 82, 70, 80, 89, 95, 52, 13, 19, 9, 79, 35, 67, 51, 39, 7, 1, 66, 8, 17, 85, 71, 97, 34, 73, 75, 6, 91, 40, 96, 65, 37, 74, 20, 68, 23, 47, 76, 55, 24, 3, 30, 22, 55, 5, 69, 86, 54, 50, 10, 59, 15, 4, 36, 38, 83, 60, 72, 63, 78, 58, 88, 93, 45, 18, 94, 44, 92, 81, 25, 26])
 # out 3
-
-
-
-#p a
-
-  #my_hash = hist_hash.select {|k,v| k != i}
-  
-  #a.product(uniq_arr).map { |x, y| my_arr.push([x, y]) if (x-y).abs < 1}
-  #p uniq_arr.product(a)
-
-  #uniq_arr.length.times do |i|
-  #  a.length.times do |j|
-  #    if  (uniq_arr[i] - a[j]).abs <= 1 
-  #      my_arr[i][j].push("k")
-  #   end
-  #  end
-  #end
-
-  #p my_arr
