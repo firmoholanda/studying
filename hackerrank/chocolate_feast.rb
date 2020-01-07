cdea966e9d9bdd4e7a4089d3c991de67e91d7d21
firmo holanda <firmo@viize.com>
2020-01-07 19:37:11 -0300

    Update chocolate_feast.rb

--- hackerrank/chocolate_feast.rb
+++ hackerrank/chocolate_feast.rb
@@ -2,25 +2,17 @@
 
 def chocolateFeast(n, c, m)
 
-  total_eten = []
-  total_warp = []
+  arr = []
+  total_eaten = total_warp = n / c
 
-  purchased_choc = n / c
-
-  total_eten.push(purchased_choc)
-  total_warp.push(purchased_choc)
-
-  i = 0
-  while total_warp.min >= m do
-    total_eten.push(total_warp[i] / m)
-    total_warp.push(total_warp[i] - (total_warp[i] / m))
-    i += 1
+  while total_warp >= m do
+    total_eaten += (total_warp / m)
+    arr.push(total_warp / m)
+    total_warp -= (total_warp / m) * (m-1)
   end
 
-  p total_eten
-  #p total_warp
-  #puts total_eten.sum
-  return total_eten.sum
+  p arr
+  return total_eaten
 
 end
 # ------------------------------------------------------------------------------------- #
@@ -31,5 +23,8 @@ end
 #chocolateFeast(15, 3, 2)
 #chocolateFeast(50, 2, 2)
 
-puts chocolateFeast(4586, 14, 98)
+#puts chocolateFeast(4586, 14, 98)
 #330
+
+puts chocolateFeast(43203, 60, 5)
+#899
