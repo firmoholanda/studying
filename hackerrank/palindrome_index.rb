83d6bf4d18ea77f0ea66f5bc46844aa7ee089976
firmo holanda <firmo@viize.com>
2020-01-08 18:25:48 -0300

    Update palindrome_index.rb

--- hackerrank/palindrome_index.rb
+++ hackerrank/palindrome_index.rb
@@ -1,12 +1,12 @@
 #https://www.hackerrank.com/challenges/palindrome-index/problem
 
 def palindromeIndex(s)
-  
-  if (s.reverse == s)
+
+  if s.reverse == s
     return -1
   else
     my_char = s.chars
-    (0...my_char.length).each do |i|
+    my_char.length.times do |i|
       my_char.delete_at i
       my_str = my_char.join
       if (my_str.reverse == my_str) then (return i) end
@@ -18,7 +18,7 @@ end
 # ------------------------------------------------------------------------------------- #
 
 
-puts palindromeIndex("aaakjhkhkj")
+puts palindromeIndex("radars")
 
 
 #8330
