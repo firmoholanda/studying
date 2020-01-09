affe18f52d2fb4dccfa4f0c655b78e31f6899a35
firmo holanda <firmo@viize.com>
2020-01-09 12:01:26 -0300

    Create lisas_workbook.rb

---
+++ hackerrank/lisas_workbook.rb
@@ -0,0 +1,18 @@
+#https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/lisa-workbook
+
+def workbook(n, k, arr)
+
+  page = []
+
+  arr.each do |i|
+    p (i % k)
+  end
+
+
+  #p book
+
+end
+# ------------------------------------------------------------------------------------- #
+
+
+workbook(5, 3, [4, 2, 6, 1, 10])
