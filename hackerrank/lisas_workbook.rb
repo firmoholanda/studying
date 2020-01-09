ca2fdb25d2f2ae246c914a0b754c70b2658928a5
firmo holanda <firmo@viize.com>
2020-01-09 19:53:40 -0300

    Update lisas_workbook.rb

--- hackerrank/lisas_workbook.rb
+++ hackerrank/lisas_workbook.rb
@@ -7,32 +7,29 @@ def workbook(n, k, arr)
   pages_in_chap = 0
 
   arr.each do |numb_of_probs|
+    # count number of pages in chap
     pages_in_chap += (numb_of_probs / k)
     if (numb_of_probs.to_f / k) - (numb_of_probs / k) > 0 then pages_in_chap += 1 end
     
-    #p pages_in_chap
     cur_prob_list = []
-    pages_in_chap.times do |p|
+    #pages_in_chap.times do |p|
+      
       numb_of_probs.times do |prob|
         cur_prob_list.push(prob+1)
-        
         page[cur_page] = cur_prob_list
-
-        if prob+1 > k
-          cur_page += 1    
+        if (prob+1 >= k) || (prob+1 >= numb_of_probs)
+          cur_prob_list = []
+          cur_page += 1        
         end
-
       end
-      
-      #p cur_prob_list
-      
-      cur_prob_list = []
-    end
-    pages_in_chap = 0
+
+    #end
+    #p pages_in_chap
+    #pages_in_chap = 0
 
   end
 
-  p page
+  p page[5]
 
 end
 # ------------------------------------------------------------------------------------- #
