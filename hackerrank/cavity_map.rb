3b84154f297295d175a8b7b9fffb73ac742963ba
firmo holanda <firmo@viize.com>
2020-01-10 13:27:48 -0300

    add cavity map

--- hackerrank/cavity_map.rb
+++ hackerrank/cavity_map.rb
@@ -2,14 +2,26 @@
 
 def cavityMap(grid)
 
-  p grid
+  #p grid.length
+
+  grid.each_with_index do |row, row_i|
+    row.each_with_index do |col, col_i|
+      #dont use the border
+      if ( (row_i != 0) && (row_i !=(grid.length-1)) ) && ( (col_i != 0) && (col_i !=(grid.length-1)) )
+        #p row_i.to_s + " " + col_i.to_s
+        p grid[row_i][col_i]
+      end
+    end
+  end
+
+  #p grid[1][1]
 
 end
 # ------------------------------------------------------------------------------------- #
 
 grid =  [ [1, 1, 1, 2],
-          [1, 1, 1, 2],
-          [1, 1, 1, 2],
-          [1, 1, 1, 2] ]
+          [1, 9, 1, 2],
+          [1, 8, 9, 2],
+          [1, 2, 3, 4] ]
 
-puts cavityMap(grid)
+cavityMap(grid)
