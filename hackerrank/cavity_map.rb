f746d3b41aaf00d4ff2c60338ff0a2ffebc0b155
firmo holanda <firmo@viize.com>
2020-01-10 11:22:31 -0300

    creatre cavity_map

---
+++ hackerrank/cavity_map.rb
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/cavity-map
+
+def cavityMap(grid)
+
+  p grid
+
+end
+# ------------------------------------------------------------------------------------- #
+
+grid =  [ [1, 1, 1, 2],
+          [1, 1, 1, 2],
+          [1, 1, 1, 2],
+          [1, 1, 1, 2] ]
+
+puts cavityMap(grid)
