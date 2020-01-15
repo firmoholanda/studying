6aae05d6d4539e8849837633b27ae0573b28611b
firmo holanda <firmo@viize.com>
2020-01-15 17:11:45 -0300

    Update rest_client.rb

--- theodinproject/rest-client/rest_client.rb
+++ theodinproject/rest-client/rest_client.rb
@@ -1,17 +1,9 @@
 require 'rest-client'
 
-response = RestClient.post "https://www.bing.com/", :param1 => "search?q=ruby"
+response = RestClient.get "https://www.bing.com/search?q=ruby"
 
-#response = RestClient.get "https://www.bing.com/search?q=ruby"
-
-#puts "Getting data from #{some_site}"
-puts "code:"
-puts response.code
-puts "\nheaders:"
-puts response.headers
-puts "\ncookies:"
-puts response.cookies
-puts "\nhistory:"
-puts response.history
-
-puts response
+puts "code:"    + response.code.to_s
+puts "headers:" + response.headers.to_s
+puts "cookies:" + response.cookies.to_s
+puts "history:" + response.history.to_s
+puts "body:"    + response.body
