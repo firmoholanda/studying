260e0283390a68cb36a7e3981bb2c8a1da3061d3
firmo holanda <firmo@viize.com>
2020-01-17 10:53:32 -0300

    add ladybugs

---
+++ hackerrank/surface_area.rb
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/3d-surface-area
+
+def surfaceArea(a)
+
+p a
+
+end
+# ------------------------------------------------------------------------------------- #
+
+surfaceArea( [ [1, 3, 4], [2, 2, 3], [1, 2, 4] ] )
