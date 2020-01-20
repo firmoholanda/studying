3c3f43901f5b24fab1047494c71d5ff18be6634d
firmo holanda <firmo@viize.com>
2020-01-20 20:05:20 -0300

    create birthday_cake_candles and divisible_sum_pairs

---
+++ hackerrank/divisible_sum_pairs.rb
@@ -0,0 +1,10 @@
+#https://www.hackerrank.com/contests/microverse-coding-challenges/challenges/game-of-thrones
+
+def divisibleSumPairs(n, k, ar)
+
+  arr.inject(Hash.new(0)) { |h,v| h[v] += 1; h }.values.max
+
+end
+# ------------------------------------------------------------------------------------- #
+
+divisibleSumPairs(n, k, ar)
