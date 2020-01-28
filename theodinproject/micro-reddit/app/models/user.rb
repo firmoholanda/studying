e80ecccbcc449d4c9e483f3f8cb01da9c8d0a30e
firmo holanda <firmo@viize.com>
2020-01-28 09:30:10 -0300

    update micro-reddit

--- theodinproject/micro-reddit/app/models/user.rb
+++ theodinproject/micro-reddit/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
--- theodinproject/micro-reddit/db/migrate/20200127213138_create_posts.rb
+++ theodinproject/micro-reddit/db/migrate/20200127213138_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
-      t.integer :user_id
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
