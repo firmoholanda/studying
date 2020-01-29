87930699585afc71c5d834d04ad529e86e60b189
firmo holanda <firmo@viize.com>
2020-01-29 16:39:44 -0300

    update set up the back end

---
+++ theodinproject/re-former/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to new_user_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+end
--- theodinproject/re-former/app/models/user.rb
+++ theodinproject/re-former/app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+
+  validates :username, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 320 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { :within => 5..30 }
+  validate :password_requirements_are_met
+
+  def password_requirements_are_met
+    rules = {
+      " must contain at least one lowercase letter"  => /[a-z]+/,
+      #" must contain at least one uppercase letter"  => /[A-Z]+/,
+      " must contain at least one digit"             => /\d+/,
+      " must contain at least one special character" => /[^A-Za-z0-9]+/
+    }
+    rules.each do |message, regex|
+      errors.add( :password, message ) unless password.match( regex )
+    end
+  end
+
 end
