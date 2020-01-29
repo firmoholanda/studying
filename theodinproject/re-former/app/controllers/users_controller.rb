0267cb36fa1840fb01c6e27598d072970ae5d4e0
firmo holanda <firmo@viize.com>
2020-01-29 18:49:30 -0300

    up

--- theodinproject/re-former/app/controllers/users_controller.rb
+++ theodinproject/re-former/app/controllers/users_controller.rb
@@ -1,21 +1,34 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit]
+  
   def new
     @user = User.new
   end
 
-  def create
-    @user = User.new(user_params)
+  # GET /users/1/edit
+  def edit
+  end
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to new_user_path, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+  def create
+     @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path, notice: 'user was successfully created.'
+    else
+      render :new
     end
+  end
+
+  private
+    
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- theodinproject/re-former/config/routes.rb
+++ theodinproject/re-former/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:new, :create]
-  
+  resources :users, only: [:new, :create, :edit]
+
 end
