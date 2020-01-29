e52d5d430359d00cccd4996647dc4847b7345fb6
firmo holanda <firmo@viize.com>
2020-01-29 11:36:13 -0300

    update re-former

--- theodinproject/re-former/db/schema.rb
+++ theodinproject/re-former/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_134433) do
+ActiveRecord::Schema.define(version: 2020_01_29_143326) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
