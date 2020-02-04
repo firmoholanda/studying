cc93c1bc4f4eff705dee1476517c2ed8ebf41f4c
firmo holanda <firmo@viize.com>
2020-02-04 19:31:10 -0300

    Update add_two_numbers.rb

--- leetcode/add_two_numbers.rb
+++ leetcode/add_two_numbers.rb
@@ -2,10 +2,13 @@
 # https://www.rubyguides.com/2017/08/ruby-linked-list/
 # https://stackoverflow.com/questions/18618447/best-linked-list-in-ruby-without-extending-array
 
-class LinkedList
+class ListNode
+  attr_accessor :value, :next
 
-  def initialize
+  def initialize(value)
+    @value = value
     @head = nil
+    @next = nil
   end
 
   def append(value)
@@ -21,124 +24,37 @@ class LinkedList
     return node if !node.next
     return node if !node.next while (node = node.next)
   end
-  
-  def append_after(target, value)
-    node           = find(target)
-    return unless node
-    old_next       = node.next
-    node.next      = ListNode.new(value)
-    node.next.next = old_next
-  end
-
-  def find(value)
-    node = @head
-    return false if !node.next
-    return node  if node.value == value
-    while (node = node.next)
-      return node if node.value == value
-    end
-  end
-
-  def delete(value)
-    if @head.value == value
-      @head = @head.next
-      return
-    end
-    node      = find_before(value)
-    node.next = node.next.next
-  end
-
-  def find_before(value)
-    node = @head
-    return false if !node.next
-    return node  if node.next.value == value
-    while (node = node.next)
-      return node if node.next && node.next.value == value
-    end
-  end
-
-  def print
-    node = @head
-    puts node
-    while (node = node.next)
-      puts node
-    end
-  end
-
-  def each
-    node = @head
-    loop do
-      break unless node
-      yield node
-      node = node.next
-    end
-  end
 
   def to_a
-    elements = []
-    current = @head
-    while current.next != nil
-      elements << current.value
-      current = current.next
+    node = @head
+    arr = []
+    arr.push(node.value)
+    while (node = node.next)
+      arr.push(node.value)
     end
-    elements << current.value
-  end
-
-  def size
-    count = 0
-    current = @head
-    while current.next != nil
-      count +=1
-      current = current.next
-    end
-    count +=1
-  end
-
-  def add_lists(l1, l2)
-    val03 = l1.to_a.join.to_i + l2.to_a.join.to_i
-
-    l3 = LinkedList.new
-    val03.to_s.chars.map(&:to_i).each do |i|
-      l3.append(i)
-    end
-    return l3
+    return arr
   end
 
 end
 
-class ListNode
-  attr_accessor :next
-  attr_reader   :value
-
-  def initialize(value)
-    @value = value
-    @next  = nil
-  end
-
-  def to_s
-    "#{@value}"
-  end
-
-  def to_i
-    @value.to_i
-  end
-
-end
+# ------------------------------------------------------------------------------------- #
 
 def add_two_numbers(l1, l2)
   
-  my_llist = LinkedList.new
-  l3 = my_llist.add_lists(l1, l2)
-  
+  #return_list = ListNode.new
+
+  p l1.to_a
+  p l1.to_a.join.to_i + l2.to_a.join.to_i
+
 end
 # ------------------------------------------------------------------------------------- #
 
-l1 = LinkedList.new
+l1 = ListNode.new("list01")
 l1.append(3)
 l1.append(4)
 l1.append(2)
 
-l2 = LinkedList.new
+l2 = ListNode.new("list02")
 l2.append(4)
 l2.append(6)
 l2.append(5)
