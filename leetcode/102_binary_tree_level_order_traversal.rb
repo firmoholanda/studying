3b6ba70e7ce3de57c12eebabb42d1daffd80284c
firmo holanda <firmo@viize.com>
2020-02-10 11:37:02 -0300

    Update 102_binary_tree_level_order_traversal.rb

--- leetcode/102_binary_tree_level_order_traversal.rb
+++ leetcode/102_binary_tree_level_order_traversal.rb
@@ -1,31 +1,61 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-# https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+# https://www.geeksforgeeks.org/level-order-tree-traversal/
 
 class TreeNode
   attr_accessor :val, :left, :right
+  
   def initialize(val)
     @val = val
     @left, @right = nil, nil
   end
+
 end
 
 # ------------------------------------------------------------------------------------- #
+def height(node)
+  
+  if node == nil
+    return 0
+  else
+    left_height = height(node.left)
+    right_height = height(node.right)
+  end
+
+  left_height > right_height ? (return left_height+1) : (return right_height+1)
+
+end
+
+def print_given_level(root, level)
+  
+  if root == nil then return end
+  
+  if level == 1
+    puts root.val
+  elsif level > 1
+    print_given_level(root.left , level-1) 
+    print_given_level(root.right , level-1) 
+  end
+
+end
+
 def level_order(root)
 
-  return_arr = []
+  #return_arr = []
+  height = height(root)
 
-  if root
-    inorder_traversal(root.left)
-    return_arr.push(root.val)
-    inorder_traversal(root.right)
-    p return_arr
+  (1..height+1).each do |i|
+    p print_given_level(root, i)
   end
 
 end
 # ------------------------------------------------------------------------------------- #
  
 my_tree = TreeNode.new(3)
-my_tree.right = TreeNode.new(2)
-my_tree.right.left = TreeNode.new(3)
+my_tree.left = TreeNode.new(9)
+my_tree.right = TreeNode.new(20)
+my_tree.right.left = TreeNode.new(15)
+my_tree.right.right = TreeNode.new(7)
 
 level_order(my_tree)
+
+#p height(my_tree)
