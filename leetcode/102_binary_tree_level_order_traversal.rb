ba8146c329efc3406feea76fbce956e15fe1de68
firmo holanda <firmo@viize.com>
2020-02-10 11:08:47 -0300

    Create 102_binary_tree_level_order_traversal.rb

---
+++ leetcode/102_binary_tree_level_order_traversal.rb
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+
+class TreeNode
+  attr_accessor :val, :left, :right
+  def initialize(val)
+    @val = val
+    @left, @right = nil, nil
+  end
+end
+
+# ------------------------------------------------------------------------------------- #
+def level_order(root)
+
+  return_arr = []
+
+  if root
+    inorder_traversal(root.left)
+    return_arr.push(root.val)
+    inorder_traversal(root.right)
+    p return_arr
+  end
+
+end
+# ------------------------------------------------------------------------------------- #
+ 
+my_tree = TreeNode.new(3)
+my_tree.right = TreeNode.new(2)
+my_tree.right.left = TreeNode.new(3)
+
+level_order(my_tree)
