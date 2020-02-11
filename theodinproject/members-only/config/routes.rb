a6dd2c98ba0e4720c10dfcd25b3402f98749b31c
firmo holanda <firmo@viize.com>
2020-02-11 19:39:47 -0300

    update members-only app

--- theodinproject/members-only/config/routes.rb
+++ theodinproject/members-only/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get     'sessions/new'
+  get     '/signup',  to: 'users#new'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  
 end
