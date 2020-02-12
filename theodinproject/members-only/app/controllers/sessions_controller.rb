95435c80342fd2be18e8a468cc32510e2013e48f
firmo holanda <firmo@viize.com>
2020-02-12 19:44:40 -0300

    update menbers-only app

--- theodinproject/members-only/app/controllers/sessions_controller.rb
+++ theodinproject/members-only/app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+
 end
