109d321473253280403a498332cc5098671ccac4
firmo holanda <firmo@viize.com>
2020-02-12 14:29:11 -0300

    create 312_burst_balloons

---
+++ leetcode/312_burst_balloons.rb
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/burst-balloons/
+
+# @param {Integer[]} nums
+# @return {Integer}
+def max_coins(nums)
+    
+end
+# ------------------------------------------------------------------------------------- #
+
+max_coins([3,1,5,8])
