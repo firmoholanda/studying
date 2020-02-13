4e7ff4ff0f3b2c4c975d7480ee3efc27c051b4fa
firmo holanda <firmo@viize.com>
2020-02-13 19:34:52 -0300

    update members-only

--- theodinproject/members-only/app/models/post.rb
+++ theodinproject/members-only/app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, presence: true, length: { maximum: 255 }
 end
