10b463f0f922266ea44e1f0187c7621b61a698cb
firmo holanda <firmo@viize.com>
2020-02-13 13:13:02 -0300

    create singup

--- theodinproject/members-only/app/controllers/sessions_controller.rb
+++ theodinproject/members-only/app/controllers/sessions_controller.rb
@@ -6,11 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
   end
 
 end
--- theodinproject/members-only/app/models/user.rb
+++ theodinproject/members-only/app/models/user.rb
@@ -25,4 +25,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
 end
