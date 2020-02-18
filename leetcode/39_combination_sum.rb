6e7b216d50326530d5e61a2d71888937b3105ef1
firmo holanda <firmo@viize.com>
2020-02-18 10:52:23 -0300

    Create 39_combination_sum.rb

---
+++ leetcode/39_combination_sum.rb
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/combination-sum/
+
+# http://nizhu.github.io/ruby-intro-site/blog/2013/07/28/numbers/
+
+# @param {Integer[]} candidates
+# @param {Integer} target
+# @return {Integer[][]}
+def combination_sum(candidates, target)
+    
+end
+# ------------------------------------------------------------------------------------- #
+
+combination_sum([1, 2, 5], 11)
