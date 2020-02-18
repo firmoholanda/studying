9d7120f44547fbbfec6c82c4ba9469427e41aa91
firmo holanda <firmo@viize.com>
2020-02-18 13:08:26 -0300

    Update 39_combination_sum.rb

--- leetcode/39_combination_sum.rb
+++ leetcode/39_combination_sum.rb
@@ -6,8 +6,38 @@
 # @param {Integer} target
 # @return {Integer[][]}
 def combination_sum(candidates, target)
+
+  #comb_arr = []
+  #(1..candidates.size).each do |i|
+  #  candidates.combination(i).each do |j|
+  #    comb_arr << j
+  #  end
+    #puts candidates.combination(i)
+  #end
+
+  #p comb_arr * candidates
+  #p comb_arr.zip(candidates).map{|x, y| x * y}
+
+  #comb_arr2 = []
+  #candidates.each do |c|
+  #  (comb_arr * c).each do |c_item|
+      #if c_item.sum == target
+  #      comb_arr2 << c_item
+      #end
+  #  end
+  #end
+
+  #p comb_arr2.
+
+  #puts candidates.combination(4).size
+  #p comb_arr * 
+
+  puts (1..candidates.size).each {|n| candidates.repeated_permutation(n).map(&:join)}
+
+
+#  p comb.to_i
     
 end
 # ------------------------------------------------------------------------------------- #
 
-combination_sum([1, 2, 5], 11)
+combination_sum([2,3,6,7], 7)
