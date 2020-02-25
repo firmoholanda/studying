6c0bbb3e7cf1f4fc37f7150c65287b4c50e0c11c
firmo holanda <firmo@viize.com>
2020-02-25 12:44:37 -0300

    update fibo

--- hackerrank/fibonacci_numbers.rb
+++ hackerrank/fibonacci_numbers.rb
@@ -7,10 +7,17 @@
 #  @cache[n] = fib(n-1) + fib(n-2)
 #end
 
-#def fib(n)
-#  phi = (Math.sqrt(5) + 1) / 2
-#  return ((phi**n) / Math.sqrt(5)).round
-#end
+# fib number of index n
+def fib(n)
+  phi = (Math.sqrt(5) + 1) / 2
+  return ((phi**n) / Math.sqrt(5)).round
+end
+
+# index of given fib number
+def fib_of(n)
+  phi = (Math.sqrt(5) + 1) / 2
+  n == 0 ? 0 : ((Math.log(n) + (Math.log(5)/2) ) / Math.log(phi)).round
+end
 
 def fibEven(n)
 
@@ -19,6 +26,8 @@ def fibEven(n)
   (2..n).each do |i|
     (my_fib[-1] > n) ? break : my_fib.push(my_fib[i-1] + my_fib[i-2])    
   end
+
+  p my_fib
 
   # remove item if greater than n (need work)
   my_fib.each do |i|
@@ -29,11 +38,30 @@ def fibEven(n)
   my_fib_even = []
   my_fib.map { |i| my_fib_even.push(i) if i.even? }
 
+  p my_fib_even
+
   return my_fib_even.sum
 
 end
 
-puts fibEven(100)
+def fibEven_version02(n)
+  
+  # initialize fib arr
+  my_fib = [0, 1]
 
-#puts i
-#puts fib(10)
+  (2..fib_of(n)).each do |i|
+    my_fib.push(my_fib[i-1] + my_fib[i-2])
+  end
+
+  p my_fib
+
+  #p fib_of(0)
+
+end
+# ------------------------------------------------------------------------------------- #
+
+#puts fibEven(100)
+
+fibEven_version02(100)
+
+#puts fib_of(100)
