8d34f57b3aa11200c26f3dc7feacc86c32f2e827
firmo holanda <firmo@viize.com>
2020-02-25 11:09:45 -0300

    add repl.it

--- repl.it/1.1_lists.rb
+++ repl.it/1.1_lists.rb
@@ -5,7 +5,7 @@ class Node
     attr_accessor :value, :next_node
     
     def initialize(value, next_node = nil)
-        @value = value
+      @value = value
       @next_node = next_node
     end
 end
