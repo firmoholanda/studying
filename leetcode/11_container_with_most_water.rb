bba429a4b9f3d4370eefe33f69a6ea1f439b7667
firmo holanda <firmo@viize.com>
2020-03-04 14:38:56 -0300

    add 338 leetcode

--- leetcode/11_container_with_most_water.rb
+++ leetcode/11_container_with_most_water.rb
@@ -2,9 +2,22 @@
 # @return {Integer}
 def max_area(height)
 
+  area = []
+  p_left = 0
+  p_right = height.size - 1
 
+  while p_left < p_right
+    area << [height[p_left], height[p_right]].min * (p_right - p_left)
+    height[p_left] > height[p_right] ? p_right -= 1 : p_left += 1
+  end
+
+  return area.max
   
 end
 # ------------------------------------------------------------------------------------- #
 
-max_area([1,8,6,2,5,4,8,3,7])
+p max_area([1,8,6,2,5,4,8,3,7])
+# 49
+
+p max_area([1,3,2,5,25,24,5])
+# 24
