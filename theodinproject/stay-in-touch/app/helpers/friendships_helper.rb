3482b87344d4c0d69bd61564b78287da4e8bfa2c
firmo holanda <firmo@viize.com>
2020-03-05 16:27:11 -0300

    up

---
+++ theodinproject/stay-in-touch/app/helpers/friendships_helper.rb
@@ -0,0 +1,12 @@
+module FriendshipsHelper
+  def friend_list
+    @friend_list = current_user.friendships.map { |friendship| friendship.passive_friend if friendship.approved }
+    @friend_list += current_user.inverse_friendships.map { |friendship| friendship.active_friend if friendship.approved }
+    @friend_list << current_user
+    @friend_list.compact
+  end
+
+  def friend?(user)
+    friend_list.include?(user)
+  end
+end
