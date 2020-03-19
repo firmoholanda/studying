5c116a584b48a054796a78fd5e7a27273a977724
firmo holanda <firmo@viize.com>
2020-03-19 11:04:31 -0300

    add course-schedule

---
+++ leetcode/207_course_schedule.rb
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/course-schedule/
+
+# @param {Integer} num_courses
+# @param {Integer[][]} prerequisites
+# @return {Boolean}
+def can_finish(num_courses, prerequisites)
+    
+end
+
+# ------------------------------------------------------------------------------------- #
