a5296e6bbdd7a64da39d045dd30c7ddf4a1fd3b3
firmo holanda <firmo@viize.com>
2020-03-31 18:10:57 -0300

    Create detect-pangram.js

---
+++ codewars/detect-pangram.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript
+
+function isPangram(string){
+  const string.map(sweetItem => {
+    return sweetItem * 2
+  })
+}
+// ------------------------------------------------------------------------------------- #
