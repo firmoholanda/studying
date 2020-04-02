b1a1e5722446c138093cbb0f1b16ba0b940438a7
firmo holanda <firmo@viize.com>
2020-04-02 11:32:12 -0300

    Create stop-gninnips-my-sdrow.js

---
+++ codewars/stop-gninnips-my-sdrow.js
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript
+
+function spinWords(str) {
+
+  let words = str.split(" ");
+  let answer = []
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= 5) {
+      answer.push(words[i].split("").reverse().join(""));
+    } else {
+      answer.push(words[i]);
+    }
+  }
+
+  console.log(answer.join(" "));
+ 
+}
+// ------------------------------------------------------------------------------------- #
+spinWords("Welcome")
+//"emocleW"
+
+//spinWords("Hey fellow warriors")
+//"Hey wollef sroirraw"
