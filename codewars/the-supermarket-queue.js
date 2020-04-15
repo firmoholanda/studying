b819ee89c1d2a941cb48449c75b4c8d82fb0dbf7
firmo holanda <firmo@viize.com>
2020-04-15 11:16:10 -0300

    Create the-supermarket-queue.js

---
+++ codewars/the-supermarket-queue.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
+
+function queueTime(customers, n) {
+  //TODO
+}
+// ------------------------------------------------------------------------------------- #
+
