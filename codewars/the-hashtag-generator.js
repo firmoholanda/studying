f3dfcdcf8cf7a674b52e677a813eec009d1d9877
firmo holanda <firmo@viize.com>
2020-04-17 11:28:36 -0300

    Update the-hashtag-generator.js

--- codewars/the-hashtag-generator.js
+++ codewars/the-hashtag-generator.js
@@ -1,5 +1,20 @@
 // https://www.codewars.com/kata/52449b062fb80683ec000024/train/javascript
 
 function generateHashtag(str) {
+  
+  hashTag = "#"
+
+  str.split(" ").forEach(function (i) {    
+    hashTag += (i.charAt(0).toUpperCase() + i.slice(1))
+  })
+
+  if (hashTag.length == 1)  {return false}
+  if (hashTag.length > 140) {return false}
+
+  return hashTag
+ 
 }
 // ------------------------------------------------------------------------------------- #
+
+generateHashtag("do we have a hashtag")
+// #DoWeHaveAHashtag
