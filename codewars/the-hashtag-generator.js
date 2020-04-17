6048e8945600b2754aadc68e2c7f7d873b032a7c
firmo holanda <firmo@viize.com>
2020-04-17 11:06:32 -0300

    Create the-hashtag-generator.js

---
+++ codewars/the-hashtag-generator.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/52449b062fb80683ec000024/train/javascript
+
+function generateHashtag(str) {
+}
+// ------------------------------------------------------------------------------------- #
