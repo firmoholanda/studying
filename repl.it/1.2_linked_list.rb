a3da79b40004dd1ccdb5f6fabea6aee75f704421
firmo holanda <firmo@viize.com>
2020-04-23 11:52:39 -0300

    Update 1.2_linked_list.rb

--- repl.it/1.2_linked_list.rb
+++ repl.it/1.2_linked_list.rb
@@ -1,47 +1,48 @@
-#https://repl.it/student/submissions/8846068?lite=true
-#https://hackernoon.com/implementing-singly-linked-list-with-ruby-om2df3ya6
+# https://repl.it/student/submissions/8846068?lite=true
+# https://hackernoon.com/implementing-singly-linked-list-with-ruby-om2df3ya6
 
 class Node
-    attr_accessor :value, :next_node
+  attr_accessor :value, :next_node
     
-    def initialize(value, next_node = nil)
-      @value = value
-      @next_node = next_node
-    end
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
 end
   
 class LinkedList
-    #setup head and tail
-    def initialize
-        @head = nil
-        @tail = nil
-    end
-  
-    def add(number)
-        #create a new node 
-        this_node = Node.new(number)
-        if @head.nil?
-            @head = this_node
-            return
-        end
+  # setup head and tail
+  def initialize
+    @head = nil
+    @tail = nil
+  end
 
-        current = @head  
-        #until current.nil means until we reach the last node
-        until current.next_node.nil?
-            current = current.next_node
-        end
+  # create a new node
+  def add(number)
+    this_node = Node.new(number)
+    if @head.nil?
+      @head = this_node
+      return
+    end
 
-        #point current(last node) to our new node
-        current.next_node = this_node
+    current = @head  
+    
+    until current.next_node.nil?      # until current.nil means until we reach the last node
+      current = current.next_node
     end
+
+    current.next_node = this_node     # point current(last node) to our new node
+  end
+
+  # get node @ index
+  def get(index)
+    current=@head
+    index.times do
+      current = current.next_node
+    end
+    return current.value
+  end
   
-    def get(index)
-        current=@head
-        index.times do
-            current=current.next_node
-        end
-        return current.value
-    end
 end
 
 #--------------------------------------------------------------------
