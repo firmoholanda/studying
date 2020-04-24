f6ae57add568daaa3f7095af53eeff21bdf4d39b
firmo holanda <firmo@viize.com>
2020-04-24 07:55:20 -0300

    Update trip_optimization.rb

--- trip_optimization.rb
+++ trip_optimization.rb
@@ -1,3 +1,27 @@
+=begin
+  
+# Instruction:
+""" 
+You have a [X]-hour layovr in paris and want to book some Trips.
+You prefer to book as few Trips as possible to enjoy the last [X]
+hours with minimum context switch. How will you arrange your time?
+Assuming that there is no time cost for transportation between Trips.
+Input:
+An array of Trips durations (hours). e.g. [1, 3, 6]
+Total remaining hours, e.g. 15
+Output:
+Minimum number of purchase. e.g. 3 (6 + 6 + 3 = 15)
+If there is no way to fill total remaining hours, return -1.
+Input Sample:
+1 3 6
+15
+Output:
+3
+"""
+  
+=end
+
+
 def minPurchases(trip_durations, total_hours)
   min_purchases = -1
 
@@ -12,8 +36,7 @@ def minPurchases(trip_durations, total_hours)
   #end
 
   #p trip_durations.combination(3).to_a
-
-  
+ 
 
   arr = []
   my_td = trip_durations
