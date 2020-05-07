9a7d0fdc298aaf9d3b683e0b8f1663f6f06acde3
firmo holanda <firmo@viize.com>
2020-05-07 12:05:43 -0300

    update tic-tac-toe javascript

--- theodinproject/tictactoe-javascript/js/script.js
+++ theodinproject/tictactoe-javascript/js/script.js
@@ -1,122 +1,117 @@
-let index = 0;
-let myLibrary = [];
+const UI = function () {
+  const cells = document.querySelectorAll(".cell");
+  const gameOverPopup = document.querySelector(".result");
+  const gameOverPopupText = document.querySelector(".winner-text");
+  const btnClosePopup = document.querySelector(".close");
+  const infoText = document.querySelector(".info");
 
-// book constructor
-function Book(title, author, pages, status = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
-}
-
-// add book to lib
-function addBookToLibrary() {
-  let addTitle = document.getElementById("new-title").value;
-  let addAuthor = document.getElementById("new-author").value;
-  let addPages = document.getElementById("new-pages").value;
-  let radios = document.getElementsByName("new-read");
-
-  radios.forEach(function (radio) {
-    if (radio.checked) addStatus = radio.value;
-  });
-
-  let newBook = new Book(addTitle, addAuthor, addPages, addStatus);
-  myLibrary.push(newBook);
-  saveLocalAndRender();
-}
-
-// render boolks on lib table
-function render(books) {
-
-  let table = document.getElementById("lib-table");
-
-  while (index < books.length) {
-    let row = table.insertRow();
-
-    let indexColumn = document.createElement("th");
-    indexColumn.setAttribute("scope", "row");
-    indexColumn.innerHTML = index + 1;
-    row.appendChild(indexColumn);
-
-    let titleColumn = row.insertCell(1);
-    titleColumn.innerHTML = books[index].title;
-
-    let authorColumn = row.insertCell(2);
-    authorColumn.innerHTML = books[index].author;
-
-    let pagesColumn = row.insertCell(3);
-    pagesColumn.innerHTML = books[index].pages;
-
-    // status btn
-    let btnColumn = row.insertCell(4);
-    if (books[index].status === false) {
-      createStatusBtn(btnColumn, "btn btn-outline-info", "reading", true);
-    } else {
-      createStatusBtn(btnColumn, "btn btn-outline-success", "finished", false);
-    }
-
-    // remove btn
-    let removeColumn = row.insertCell(5);
-    let removeBtn = document.createElement("button");
-    removeColumn.appendChild(removeBtn);
-    removeBtn.setAttribute("class", "btn btn-outline-danger");
-    removeBtn.setAttribute("id", `${index}`);
-    removeBtn.innerHTML = "remove";
-    row.appendChild(removeColumn);
-
-    removeBtn.addEventListener("click", function () {
-      myLibrary.splice(this.id, 1);
-      let tableRows = table.getElementsByTagName('tr');
-      while (index > 0) {
-        table.removeChild(tableRows[index - 1]);
-        index--;
-      };
-      saveLocalAndRender();
+  // Empties cell contexts and hooks click listeners
+  const initCells = () => {
+    [...cells].forEach(cell => {
+      cell.textContent = "";
+      cell.addEventListener("click", handleClick);
     });
-    
-    index++;
+  };
 
-    function createStatusBtn(btnColumn, btnClass, btnText, btnStatus) {
-      let statusBtn = document.createElement("button");
-      btnColumn.appendChild(statusBtn);
-      statusBtn.setAttribute("class", btnClass);
-      statusBtn.setAttribute("id", `${index}`);
-      statusBtn.innerHTML = btnText;
+  // Removes click listeners of cells
+  const deinitCells = () => {
+    [...cells].forEach(cell => cell.removeEventListener("click", handleClick));
+  };
 
-      statusBtn.addEventListener("click", function () {
-        books[this.id].status = btnStatus;
-        let tableRows = table.getElementsByTagName('tr');
-        while (index > 0) {
-          table.removeChild(tableRows[index - 1]);
-          index--;
-        };
-        saveLocalAndRender();
-      });
-    }
+  const endGame = () => {
+    setInfoText("GAME OVER!");
+    deinitCells();
+    showGameOverPopup();
+  };
 
+  // Updates info text with given text
+  const setInfoText = text => {
+    infoText.textContent = text;
+  };
+
+  // Updates info text with current player info
+  const updateCurrentPlayer = player => {
+    setInfoText(`${player} will make a move`);
+  };
+
+  // Makes game over popup visible
+  const showGameOverPopup = () => {
+    gameOverPopupText.textContent = `${currentPlayer} WINS!`;
+    gameOverPopup.style.display = "block";
+  };
+
+  // Makes game over popup hidden
+  const hideGameOverPopup = () => {
+    gameOverPopup.style.display = "none";
+  };
+
+  // Makes given cell blink to indicate invalid move
+  const blinkCell = cell => {
+    cell.style.animation = "blink .6s 2";
+    setTimeout(() => (cell.style.animation = ""), 1200);
+  };
+
+  btnClosePopup.onclick = hideGameOverPopup;
+  return { initCells, updateCurrentPlayer, blinkCell, endGame };
+};
+
+const ui = UI();
+const winningPatterns = [
+  // Horizontal
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // Vertical
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // Diagonal
+  [0, 4, 8],
+  [6, 4, 2]
+];
+
+let currentCells = [];
+let currentPlayer = "";
+
+function startGame() {
+  currentCells = Array.from(Array(9).keys());
+  currentPlayer = "X";
+  ui.updateCurrentPlayer(currentPlayer);
+  ui.initCells();
+}
+
+function handleClick() {
+  const cell = event.target;
+  const id = cell.dataset.id;
+  if (isCellValid(id)) {
+    cell.textContent = currentPlayer;
+    currentCells[id] = currentPlayer;
+    if (checkWin()) ui.endGame();
+    else togglePlayer();
+  } else {
+    ui.blinkCell(cell);
   }
 }
 
-// save to local storage and render lib
-function saveLocalAndRender() {
-  localStorage.setItem("myLib", JSON.stringify(myLibrary));
-  render(myLibrary);
+function isCellValid(id) {
+  return currentCells[id] == id;
 }
 
-// populateBooks
-function populateBooks() {
-  myLibrary.push(new Book("book01", "author01", 11, true));
-  myLibrary.push(new Book("book02", "author02", 22, false));
-  myLibrary.push(new Book("book03", "author03", 33, true));
+function togglePlayer() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  ui.updateCurrentPlayer(currentPlayer);
 }
 
-// store lib in localstorage
-if (localStorage.getItem("myLib") === null) {
-  populateBooks();
-  localStorage.setItem("myLib", JSON.stringify(myLibrary));
-} else {
-  myLibrary = JSON.parse(localStorage.getItem("myLib"));
+function checkWin() {
+  for (let i = 0; i < winningPatterns.length; i++) {
+    let pattern = winningPatterns[i];
+    let [a, b, c] = pattern;
+    if (
+      currentCells[a] === currentCells[b] &&
+      currentCells[b] === currentCells[c]
+    ) {
+      return true;
+    }
+  }
+  return false;
 }
-
-// render books
-render(myLibrary);
