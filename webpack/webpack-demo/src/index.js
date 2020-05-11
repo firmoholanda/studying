37481564e52c3ecaed265b439d8c3c6cbb2721dd
firmo holanda <firmo@viize.com>
2020-05-11 18:07:19 -0300

    up

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
   
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  
+  element.appendChild(myIcon);
 
   return element;
 }
