0fc6e47f72997661e3867f9854e2b98b165954e7
firmo holanda <firmo@viize.com>
2020-05-19 11:47:36 -0300

    update happy-ladybugs

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -5,14 +5,13 @@ import Icon from './icon.png';
 function component() {
   const element = document.createElement('div');
   
-  element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['hello firmo', 'webpack testing'], ' ');
   element.classList.add('hello');
 
-  // Add the image to our existing div.
+  // add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
-  
-  element.appendChild(myIcon);
+  //element.appendChild(myIcon);
 
   return element;
 }
