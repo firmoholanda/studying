78806fa55755354b2801f7c1b9671d4f20abe190
firmo holanda <firmo@viize.com>
2020-05-21 15:28:10 -0300

    remove navbar

--- theodinproject/todo-list-js-webpack/src/index.js
+++ theodinproject/todo-list-js-webpack/src/index.js
@@ -2,43 +2,36 @@
 import './css/style.css';
 
 // import modules
-import navbar from './navbar';
-import footer from './footer';
-import home from './tab/home';
-import menu from './tab/menu';
-import contact from './tab/contact';
-import about from './tab/about';
-
+import navbar from './view/navbar';
+import footer from './view/footer';
+import home from './view/tab/home';
+import projects from './view/tab/projects';
+import about from './view/tab/about';
 
 const main = document.querySelector('#content');
 
-main.insertAdjacentHTML('beforeEnd', navbar.outerHTML);
+main.insertAdjacentHTML('beforeEnd', navbar);
 main.insertAdjacentHTML('beforeEnd', '<div class="activeInfo"></div>');
-main.insertAdjacentHTML('beforeEnd', footer.outerHTML);
-
+main.insertAdjacentHTML('beforeEnd', footer);
 
 const activeInfo = document.querySelector('.activeInfo');
-activeInfo.innerHTML = home.outerHTML;
+activeInfo.innerHTML = home;
 
 
 const nav = document.querySelector('.navbar');
-
 nav.addEventListener('click', e => {
   if (e.target && e.target.classList.contains('nav-link')) {
     const selectedMenu = e.target.outerText;
 
     switch (selectedMenu) {
-      case 'menu':
-        activeInfo.innerHTML = menu.outerHTML;
-        break;
-      case 'contact':
-        activeInfo.innerHTML = contact.outerHTML;
+      case 'projects':
+        activeInfo.innerHTML = projects;
         break;
       case 'about':
-        activeInfo.innerHTML = about.outerHTML;
+        activeInfo.innerHTML = about;
         break;
       default:
-        activeInfo.innerHTML = home.outerHTML;
+        activeInfo.innerHTML = home;
     }
   }
 });
---
+++ theodinproject/todo-list-js-webpack/src/view/tab/projects.js
@@ -0,0 +1,7 @@
+const projects = 
+
+`
+  <p class=''>this is a simple todo list app made with javascript</p>
+`
+
+export default projects;
