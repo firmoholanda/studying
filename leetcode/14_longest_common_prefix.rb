0e6d31710027e08556ce81602ed2fdd441be7fae
firmo holanda <firmo@viize.com>
2020-06-15 15:10:25 -0300

    update from surface

---
+++ leetcode/14_longest_common_prefix.rb
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+# not finished
+
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+
+  #strs.each do |w|
+  #  p w
+  #end
+
+  strs_sort = strs.sort
+  #p strs_sort
+  #p strs[0]["z"]
+
+end
+# ------------------------------------------------------------------------------------- #
+
+longest_common_prefix(["flower","flow","flight"])
+#"fl"
+
