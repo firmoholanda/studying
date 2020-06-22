eec9a41cc099462585621db8eed62dc609ce4b20
firmo holanda <firmo@viize.com>
2020-06-22 11:55:23 -0300

    add remove zeroes

---
+++ leetcode/283_move_zeroes.rb
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/move-zeroes/
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+def move_zeroes(nums)
+
+  #count = 0
+  #out_arr = []
+
+  #nums.each { |n| n != 0 ? (out_arr << n) : (count += 1) }
+  #count.times {out_arr << 0}
+
+  #return out_arr
+  
+
+  count = nums.count(0)             # counts total of 0
+  nums.delete_if { |x| x == 0 }     # remove 0s
+  count.times {nums << 0}           # add 0s to the end
+
+  return nums
+
+end
+# ------------------------------------------------------------------------------------- #
+
+p move_zeroes([0, 1, 0, 3, 12])
+# [1,3,12,0,0]
