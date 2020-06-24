caeef68a780a6860a1271c9ea7ebd1c8b7bc04bb
firmo holanda <firmo@viize.com>
2020-06-24 11:24:01 -0300

    add 189_rotate_array

---
+++ leetcode/189_rotate_array.rb
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/rotate-array/
+
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+
+  nums.pop(k) + nums
+  
+end
+# ------------------------------------------------------------------------------------- #
+
+p rotate([1,2,3,4,5,6,7], 3)
+#[5,6,7,1,2,3,4]
+
+p rotate([-1,-100,3,99], 2)
+#[3,99,-1,-100]
