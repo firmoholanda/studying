af4534276b479f6e67d7c1ec0fdab75fd1505cd9
firmo holanda <firmo@viize.com>
2020-07-09 16:57:19 -0300

    up

---
+++ microverse/course/projects/05 javascript/weather-app-js-webpack/src/view/search.js
@@ -0,0 +1,14 @@
+const search = `
+<div class="row py-4 d-flex justify-content-center">
+  <div class="input-group col-md-4 ">
+    <input class="form-control py-2" type="search" placeholder="search your city" id="searchImput">
+    <span class="input-group-append">
+      <button class="btn btn-outline-secondary" type="button" id="searchBtn">
+        <i class="fa fa-search"></i>
+      </button>
+    </span>
+  </div>
+</div>
+`;
+
+export default search;
---
+++ microverse/course/projects/05 javascript/weather-app-js-webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  performance: { hints: false },
+};
