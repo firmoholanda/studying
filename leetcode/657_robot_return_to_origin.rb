f8a21e6534dd4b48b426cf9dd4fa97ea7b21285c
firmo holanda <firmo@viize.com>
2020-08-07 11:20:07 -0300

    Create 657_robot_return_to_origin.rb

---
+++ leetcode/657_robot_return_to_origin.rb
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/robot-return-to-origin/
+
+# @param {String} moves
+# @return {Boolean}
+def judge_circle(moves)
+
+  #move_hash = moves.chars.tally
+  move_hash = moves.chars.inject(Hash.new(0)) { |total, e| total[e] += 1 ; total}
+
+  move_hash["U"] == move_hash["D"] && move_hash["L"] == move_hash["R"]
+    
+end
+# ------------------------------------------------------------------------------------- #
+
+p judge_circle("UD")
+
