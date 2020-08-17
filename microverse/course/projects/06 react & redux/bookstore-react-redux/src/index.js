64065dd463293b5ef9fc90020b7d0dff583f2dc1
firmo holanda <firmo@viize.com>
2020-08-17 16:07:07 -0300

    up bookstore-react-redux

---
+++ microverse/course/projects/06 react & redux/bookstore-react-redux/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import rootReducer from './reducers';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+var faker = require('faker');
+
+const booksArray = [
+  {
+    id: (Math.random() * (10 ** 9)),
+    title: faker.lorem.words(),
+    category: 'Action',
+  },
+  {
+    id: (Math.random() * (10 ** 9)),
+    title: faker.lorem.words(),
+    category: 'Learning',
+  },
+  {
+    id: (Math.random() * (10 ** 9)),
+    title: faker.lorem.words(),
+    category: 'Horror',
+  },
+];
+
+const initState = {
+  books: booksArray,
+};
+
+const store = createStore(rootReducer, initState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
