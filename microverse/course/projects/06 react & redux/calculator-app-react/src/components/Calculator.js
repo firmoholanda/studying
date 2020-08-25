f9051fa3e47d651efde7514186f299006852df3c
firmo holanda <firmo@viize.com>
2020-08-25 11:53:50 -0300

    up

---
+++ microverse/course/projects/06 react & redux/calculator-app-react/src/components/Calculator.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Display from './Display';
+
+import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
+
+function Calculator() {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = buttonName => {
+    setState(Calculate(state, buttonName));
+  };
+
+  return (
+    <div className="calculator">
+      <Display result={state.next || state.total || '0'} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+}
+
+export default Calculator;
