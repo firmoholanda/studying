9d5523631d5f649c2200a85b2fc5d387bf1c54b0
firmo holanda <firmo@viize.com>
2020-08-25 11:53:50 -0300

    up

---
+++ microverse/course/projects/06 react & redux/calculator-app-react/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Jumbotron from './Jumbotron';
+import Calculator from './Calculator';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Jumbotron />
+      <Calculator />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
