4142f30e77a5bf6e4316e2384c0bf1d66181d21b
firmo holanda <firmo@viize.com>
2020-09-09 12:32:51 -0300

    up

--- .git-rewrite/t/hackerrank/convertCelsiusToFahrenheit.js
+++
@@ -1,7 +0,0 @@
-
-function convertToF(celsius) {
-  let fahrenheit = ( celsius*(9/5) )+ 32;
-  return fahrenheit;
-}
-
-console.log ( convertToF(30) );
--- .git-rewrite/t/theodinproject/blogger/app/models/tag.rb
+++
@@ -1,4 +0,0 @@
-class Tag < ApplicationRecord
-  has_many :taggings, dependent: :destroy
-  has_many :articles, through: :taggings
-end
