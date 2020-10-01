298b41bd4794dabed7f3a42062c3b134af258aa4
firmo holanda <firmo@viize.com>
2020-10-01 18:28:17 -0300

    Update toeplitz_matrix.rb

--- pramp/toeplitz_matrix.rb
+++ pramp/toeplitz_matrix.rb
@@ -2,38 +2,25 @@
 
 def function(mat)
 
-  row_count = mat.size
-  col_count = mat[0].size
   arr = []
-  
-  row_count.times do |r|
-    col_count.times do |c|
-      arr << mat[r][c]
-    end
-  end
-
-  (arr.size - (col_count+1)).times do |i|
-    p i
-    p arr[i] == arr[i+col_count+1]
-  end
-
-  
-  #p arr
-  #p arr[0]
-  #p arr[col_count+1]
-  
+  is_sub_set = []
+  row_count = mat.size
     
+  row_count.times do |r|
+    arr << mat[r].drop(r)   
+  end
+
+  (arr.size-1).downto(1) do |i|
+    is_sub_set << (arr[2] - arr[1]).empty?
+  end
+
+  p is_sub_set.all?
+
 end
 # ------------------------------------------------------------------------------------- #
 
 function([[1,2,3,4], [5,1,2,3], [6,5,1,2]])
 # true
 
-#function([[1,2,3,4], [5,1,9,3], [6,5,1,2]])
+function([[1,2,3,4], [5,1,9,3], [6,5,1,2]])
 # false
-
-mat[0][0] == mat[1][1] == mat[2][2]
-
-mat[0][1] == mat[1][2] == mat[2][3]
-
-mat[0][2] == mat[1][3]
