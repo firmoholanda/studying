55e408549633d61cf611d5798b82243e702542de
firmo holanda <firmo@viize.com>
2020-10-27 12:45:41 -0300

    Create min_window_substring.rb

---
+++ coderbyte/min_window_substring.rb
@@ -0,0 +1,13 @@
+# https://coderbyte.com/editor/Min%20Window%20Substring:Ruby
+
+def MinWindowSubstring(strArr)
+
+
+  return strArr
+
+end
+
+# ------------------------------------------------------------------------------------- #
+
+p MinWindowSubstring(["ahffaksfajeeubsne", "jefaa"])
+# aksfaje
